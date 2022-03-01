4c6ad4be4c63d5fe1edbdecff7206b8f4bfa9258
Stefano Casasola <ste.casasola@gmail.com>
2022-03-01 11:44:35 +0100

    Third break, connecting props to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap'
+import './App.css'
+import { connect } from 'react-redux'
+// connect is a function capable of linking your component to the Redux Store
+// every component you want to link needs to import the connect function and use it!
 
-function App() {
+// mapStateToProps is a function returning an object
+const mapStateToProps = (state) => ({
+  // every property of this object is going to be a prop for your component
+  count: state.count,
+  // count is now *magically* a prop for your component!
+  name: 'Rajib',
+})
+
+// quick & dirty way: takes all the properties of the state object and makes them props for your component
+// not very recommended...
+// const mapStateToProps = (state) => state
+
+// "classic" way of writing a function returning an object:
+// const mapStateToProps = () => {
+//   return {
+//     // put your properties here
+//   }
+// }
+
+// mapDispatchToProps is a function returning an object
+const mapDispatchToProps = (dispatch) => ({
+  incrementCounter: () => {
+    dispatch({
+      type: 'INCREMENT_COUNTER',
+    })
+  },
+})
+
+const App = ({ count, incrementCounter }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Button onClick={incrementCounter}>+</Button>
+        <h3>{count}</h3>
+        <Button>-</Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+// the connect() function decides at which level you want this component to be able to interact to the redux store
+// you have two options:
+// 1) "READ ONLY MODE"
+// 2) "READ/WRITE MODE"
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -8,6 +8,15 @@ import { initialState } from '../store'
 const mainReducer = (state = initialState, action) => {
   // we have to make sure that in every case we return the new state of the application
   switch (action.type) {
+    case 'INCREMENT_COUNTER':
+      return {
+        // every object you return from the reducer function
+        // is the new state of the application
+        // as a primary rule, you don't want to lose anything on the road
+        ...state,
+        count: state.count + 1,
+      }
+
     default:
       return state
     // we don't want to break anything if the action that was just dispatched
